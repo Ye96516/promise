818efc7503998d7b45a078676abd2e5da23022a6
Ye96516 <162856784+Ye96516@users.noreply.github.com>
2025-08-10 14:59:22 +0800

    数据化金钱等的值

--- scenes/start_travel/start_travel.gd
+++ scenes/start_travel/start_travel.gd
@@ -2,12 +2,20 @@ extends Control
 
 @export var bg:AudioStream
 
+@export var time:int
+@export var wake:int
+@export var health:int
+@export var gc:int
+
+@onready var time_wake_value: Label = %TimeWakeValue
+@onready var money_health_value: Label = %MoneyHealthValue
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	time_wake_value.text="时间："+str(time)+"\n"+"觉醒值"+str(wake)+r"\100"
+	money_health_value.text="金钱："+str(gc)+"\n"+"健康值"+str(health)+r"\100"
 	if bg:
 		AudioPlayer.play(bg)
-	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
