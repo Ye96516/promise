3348be540e22aae44aa9012318b3412ba7a49bd2
Ye96516 <162856784+Ye96516@users.noreply.github.com>
2025-08-11 14:09:23 +0800

    修复bug并且重构代码

--- scenes/info_panel/info_panel.gd
+++ scenes/info_panel/info_panel.gd
@@ -27,73 +27,64 @@ func update_info(co:int):
 	head.text=data["概括"]
 	stem.text=data["题目"]
 	about=data["关联"].split(";")
-	var content:PackedStringArray=data["选项"].split(";")
-	content_a=content[0].split("|")
-	content_b=content[1].split("|")
-	print(content_a)
-	if content.size()!=2:
+	
+	var options:PackedStringArray=data["选项"].split(";")
+	content_a=options[0].split("|")
+	content_b=options[1].split("|")
+
+	_set_button_content(button,content_a)
+	_set_button_content(button_2,content_b)
+	if options.size()!=2:
 		button_3.visible=true
-		content_c=content[2].split("|")
-		if content_c.size()==4:
-			button_3.text=content_c[0]+"\n"+content_c[1]+"  "+content_c[2]+"\n"\
-			+content_c[3]
-		if content_c.size()==5:
-			button_3.text=content_c[0]+"\n"+content_c[1]+"  "+content_c[2]+"\n"\
-			+content_c[3]+"  "+content_c[4]
+		content_c=options[2].split("|")
+		_set_button_content(button_3,content_c)
 	else:
 		button_3.visible=false
-	if content_a.size()==4:
-		button.text=content_a[0]+"\n"+content_a[1]+"  "+content_a[2]+"\n"\
-		+content_a[3]
-	if content_a.size()==5:
-		button.text=content_a[0]+"\n"+content_a[1]+"  "+content_a[2]+"\n"\
-		+content_a[3]+"  "+content_a[4]
-	if content_b.size()==4:
-		button_2.text=content_b[0]+"\n"+content_b[1]+"  "+content_b[2]+"\n"\
-		+content_b[3]
-	if content_b.size()==5:
-		button_2.text=content_b[0]+"\n"+content_b[1]+"  "+content_b[2]+"\n"\
-		+content_b[3]+"  "+content_b[4]
+		
+#此函数专门用来适配按钮文本
+func _set_button_content(btn: Button, parts: PackedStringArray) -> void:
+	match parts.size():
+		3:
+			btn.text = "%s\n%s  %s" % [parts[0], parts[1], parts[2]]
+		4:
+			btn.text = "%s\n%s  %s\n%s" % [parts[0], parts[1], parts[2], parts[3]]
+		5:
+			btn.text = "%s\n%s  %s\n%s  %s" % [parts[0], parts[1], parts[2], parts[3], parts[4]]
+		_:
+			# 处理意外情况
+			push_warning("Unexpected content size: %d for button %s" % [parts.size(), btn.name])
+			btn.text = "\n".join(parts)
 
 func _on_button_pressed() -> void:
+	#content_a的示例
 	#["A", "你决定选择飞机从北京出发前往西藏，快速进入高原导致严重高反", "4"]
-	var about_a=about[0].split("&")
-	for i in content_a.size()-1:
-		match content_a[i+1].substr(0,2):
-			"金钱":
-				Global.gc+=int(content_a[i+1].substr(2,-1))
-			"时间":
-				Global.date+=int(content_a[i+1].substr(2,-1))
-			"健康":
-				Global.health+=int(content_a[i+1].substr(2,-1))
-			"觉醒":
-				Global.wake+=int((content_a[i+1].substr(3,-1)).reverse().substr(1).reverse())
-	update_info(int(about_a[2])-1)
+	_handle_button_selection(content_a,0)
 	
 func _on_button_2_pressed() -> void:
-	var about_b=about[1].split("&")
-	for i in content_b.size()-1:
-		match content_b[i+1].substr(0,2):
-			"金钱":
-				Global.gc+=int(content_b[i+1].substr(2,-1))
-			"时间":
-				Global.date+=int(content_b[i+1].substr(2,-1))
-			"健康":
-				Global.health+=int(content_b[i+1].substr(2,-1))
-			"觉醒":
-				Global.wake+=int((content_b[i+1].substr(3,-1)).reverse().substr(1).reverse())
-	update_info(int(about_b[2])-1)
+	_handle_button_selection(content_a,1)
 	
 func _on_button_3_pressed() -> void:
-	var about_c=about[1].split("&")
-	for i in content_c.size()-1:
-		match content_c[i+1].substr(0,2):
-			"金钱":
-				Global.gc+=int(content_c[i+1].substr(2,-1))
-			"时间":
-				Global.date+=int(content_c[i+1].substr(2,-1))
-			"健康":
-				Global.health+=int(content_c[i+1].substr(2,-1))
-			"觉醒":
-				Global.wake+=int((content_c[i+1].substr(3,-1)).reverse().substr(1).reverse())
-	update_info(int(about_c[2])-1)
+	_handle_button_selection(content_a,2)
+
+func _apply_effects(effects: Array) -> void:
+	for effect in effects:
+		match effect.substr(0, 2):
+			"金钱": Global.gc += int(effect.substr(2))
+			"时间": Global.date += int(effect.substr(2))
+			"健康": Global.health += int(effect.substr(2))
+			"觉醒": Global.wake+=int((effect.substr(3,-1)).reverse().substr(1).reverse())
+				
+func _handle_button_selection(content_arr: Array, about_index: int) -> void:
+	# 检查是否为"晚"选项
+	if content_arr.size() > 1 && content_arr[1].ends_with("晚"):
+		print(content_arr)
+	
+	# 从索引1开始跳过第一个文本元素
+	var effects = content_arr.slice(1) 
+	_apply_effects(effects)
+	
+	# 更新下一题信息
+	var about_parts = about[about_index].split("&")
+	if about_parts.size() >= 3:
+		var next_idx = int(about_parts[2]) - 1
+		update_info(next_idx)
